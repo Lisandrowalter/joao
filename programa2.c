25e8341b502ddbfc55f59b214aca9b6c64571a64
Lisandrowalter <91380793+Lisandrowalter@users.noreply.github.com>
2021-09-25 16:19:18 -0300

    Aula 1

---
+++ programa2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf ("Lisandro walter") ;
+	return 0 ;
+}	
---
+++ programa3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int idade = 46;
+	printf("A idade é %d.",idade);
+	return 0;
+}
